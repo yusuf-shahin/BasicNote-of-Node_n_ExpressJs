e3cc97292d33c1ba6c0b04335f7955d5e6998b00
Yusuf <usuf7hahin@gmail.com>
2024-08-22 21:51:24 +0600

    npm on process

--- basic node/12-http.js
+++ basic node/12-http.js
@@ -1,34 +1,26 @@
-const http = require('http')
+//* Base module
+const http = require("http");
 
 const server = http.createServer((req, res) => {
-  //   if (req.url === '/') {
-  //     res.end('Welcome to our home page')
-  //   }
-  //   if (req.url === '/about') {
-  //     res.end('Here is our short history')
-  //   }
-  //   res.end(`
-  //   <h1>Oops!</h1>
-  // <p>We can't seem to find the page you are looking for</p>
-  // <a href="/">back home</a>
-  //   `)
-  // ###################################
-  // ###################################
-  //
-  //  IF YOU GET ERRORS WHILE USING ABOVE SETUP,
-  // SWITCH TO IF, ELSE IF, ELSE (BELOW)
-  // WE COVER THE CAUSE, LATER IN EXPRESS TUTORIAL
-  if (req.url === '/') {
-    res.end('Welcome to our home page')
-  } else if (req.url === '/about') {
-    res.end('Here is our short history')
+  //# create my first server...
+  // res.write("Welcome, this is my first web page by using node");
+  // res.end();
+  //# routing different different by page in server...
+  if (req.url === "/") {
+    res.end("Welcome to our home page");
+  } else if (req.url === "/about") {
+    res.end("Hello my name is yusuf Shahin");
+  } else if (req.url === "/address") {
+    res.end("9 no. Nobipur union, Senbag, Noakhali");
   } else {
+    //! if url are wrong...
     res.end(`
     <h1>Oops!</h1>
     <p>We can't seem to find the page you are looking for</p>
     <a href="/">back home</a>
-    `)
+    `);
   }
-})
+});
 
-server.listen(5000)
+server.listen(1997);
+// http://localhost:1997/
--- basic node/app.js
+++ basic node/app.js
@@ -1,1 +1,8 @@
-console.log('Welcome to Node Tutorial')
+console.log("Welcome to Node Tutorial");
+
+//? Test lodash
+
+const lodash = require("lodash");
+const itemArray = [1, 2, 3, [4, 5, [6, 7, [8]]]];
+const newItenArray = lodash.flattenDeep(itemArray);
+console.log(newItenArray);
